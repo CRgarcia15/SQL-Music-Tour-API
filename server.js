9d9d36a9228f1b792cb0c0be35c5c75c42809fb5
CRgarcia15 <cg58708@gmial.com>
2022-08-30 18:09:30 -0400

    update

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.get("/", (req, res) => {
 const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
 
-const eventsController = require('./controllers/event_controller')
+const eventsController = require('./controllers/events_controller')
 app.use('/events', eventsController)
 
-const stagesController = require('./controllers/stage_controller')
+const stagesController = require('./controllers/stages_controller')
 app.use('/stages', stagesController)
 
 // LISTEN
